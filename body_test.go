3250a2008e8689aacc9b941aa257b1ffbdad5a07
Jacob Gunther <jacobgunther03+github@gmail.com>
2023-05-01 17:32:37 -0500

    Fix GitHub Actions

--- body_test.go
+++ body_test.go
@@ -2,55 +2,12 @@ package skin_test
 
 import (
 	"fmt"
-	"image"
-	"image/draw"
 	"image/png"
 	"os"
 	"testing"
 
 	"github.com/mineatar-io/skin-render"
 )
-
-func TestCustomSkin(t *testing.T) {
-	f, err := os.Open("test.png")
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rawImg, err := png.Decode(f)
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if err = f.Close(); err != nil {
-		t.Fatal(err)
-	}
-
-	img := image.NewNRGBA(rawImg.Bounds())
-	draw.Draw(img, rawImg.Bounds(), rawImg, image.ZP, draw.Src)
-
-	output := skin.RenderHead(img, skin.Options{
-		Scale:   8,
-		Overlay: true,
-		Slim:    false,
-	})
-
-	f, err = os.OpenFile("test-body.png", os.O_CREATE|os.O_RDWR, 0777)
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if err = png.Encode(f, output); err != nil {
-		t.Fatal(err)
-	}
-
-	if err = f.Close(); err != nil {
-		t.Fatal(err)
-	}
-}
 
 func TestFullBodySteve(t *testing.T) {
 	rawSkin := skin.GetDefaultSkin(false)
